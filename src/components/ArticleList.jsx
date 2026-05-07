cea3ea00a40d0918d1af999e8abfa15725f32565
salma <swsalama518@gmail.com>
2026-05-06 21:43:01 -0400

    updates

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -2,16 +2,15 @@ import Article from "./Article";
 
 function ArticleList(props) {
   const articles = props.articles || [];
-  const articleComponents = props.articles.map((article, index) => {
-    return (
+
+  const articleComponents = props.articles.map((article, index) => (
       <Article
         key={index}
         title={article.title}
         date={article.date}
         preview={article.preview}
       />
-    );
-  });
+  ));
 
   return <main>{articleComponents}</main>;
 }
